55e35dd11de8b5d017003d7a7f5f4a239f37d8ed
Adrian <61416203+mainpyp@users.noreply.github.com>
2022-07-11 23:25:20 +0200

    Update app.R

--- R/app.R
+++ R/app.R
@@ -2,85 +2,96 @@ source("global.R")
 source("interactive_table_functions.R")
 
 all_datasets <- as.list(as.data.table(data()$results)$Item)
-all_datasets <- sapply(as.list(as.data.table(data()$results)$Item), str_split, pattern=" ")
-all_datasets <- sapply(all_datasets,"[[", 1)
+all_datasets <-
+  sapply(as.list(as.data.table(data()$results)$Item), str_split, pattern =
+           " ")
+all_datasets <- sapply(all_datasets, "[[", 1)
 
 ui <- fluidPage(
-    selectInput("Data", label = "Select Datatable",
-              choices = all_datasets, selected = "mtcars"),
-    selectInput("n_cont", label = "Select Continous Threshold",
-                choices = 3:35, selected = 19),
-    checkboxInput("verbose", "Show if data not list"),
-    uiOutput("modals"),
-    DTOutput("table")
+  selectInput(
+    "Data",
+    label = "Select Datatable",
+    choices = all_datasets,
+    selected = "mtcars"
+  ),
+  selectInput(
+    "n_cont",
+    label = "Select Continous Threshold",
+    choices = 3:35,
+    selected = 19
+  ),
+  checkboxInput("verbose", "Show if data not list"),
+  uiOutput("modals"),
+  DTOutput("table")
 )
 
-server <- function(input, output, session){
-      observeEvent(c(input$Data, input$n_cont), {
-
-        data_id <- input$Data
-        threshold <- input$n_cont
-
-        dat <- tryCatch(
-          {
-            scraped <- get( data_id  )
-            type_scraped <- typeof(scraped)
-            
-            as.data.table(scraped)
-            
-          },
-          warning= function(cond) {
-            message(paste0(data_id, " not found."))
-            message(cond)
-            return()
-          })
-        
-        type_of_dat <- typeof(get(data_id))
-        
-        tryCatch(
-          {
-            as.data.table(dat)
-          },
-          warning= function(cond) {
-            shinyalert(sprintf("%s not convertable to data.table", data_id))
-            message(cond)
-            return()
-          })
-        
-        if(type_of_dat != "list" && input$verbose) {
-          shinyalert(sprintf("%s not a list, but of type %s", data_id, type_of_dat))
-        }
-
-        if(length(dat) == 0) {
-          shinyalert(sprintf("%s is empty", data_id))
-          return()
-        }
-        if(length( colnames(dat) ) > 1) {
-          colnames(dat) <- gsub("-", "_", colnames(dat))
-        }
-
-        buttons <- create_buttons(dat, file = data_id)
-
-        output[["table"]] <- DT::renderDataTable({
-          create_datatable(dat, buttons)
+server <- function(input, output, session) {
+  observeEvent(c(input$Data, input$n_cont), {
+    data_id <- input$Data
+    threshold <- input$n_cont
+    
+    dat <- tryCatch({
+      scraped <- get(data_id)
+      type_scraped <- typeof(scraped)
+      
+      as.data.table(scraped)
+      
+    },
+    warning = function(cond) {
+      message(paste0(data_id, " not found."))
+      message(cond)
+      return()
+    })
+    
+    type_of_dat <- typeof(get(data_id))
+    
+    tryCatch({
+      as.data.table(dat)
+    },
+    warning = function(cond) {
+      shinyalert(sprintf("%s not convertable to data.table", data_id))
+      message(cond)
+      return()
+    })
+    
+    if (type_of_dat != "list" && input$verbose) {
+      shinyalert(sprintf("%s not a list, but of type %s", data_id, type_of_dat))
+    }
+    
+    if (length(dat) == 0) {
+      shinyalert(sprintf("%s is empty", data_id))
+      return()
+    }
+    if (length(colnames(dat)) > 1) {
+      colnames(dat) <- gsub("-", "_", colnames(dat))
+    }
+    
+    buttons <- create_buttons(dat, file = data_id)
+    
+    output[["table"]] <- DT::renderDataTable({
+      create_datatable(dat, buttons)
+    })
+    
+    # modals ####
+    output[["modals"]] <- renderUI({
+      create_modals(dat, file = data_id, threshold)
+    })
+    
+    
+    # plots in modals ####
+    for (i in 1:ncol(dat)) {
+      local({
+        ii <- i
+        output[[paste0("plot", ii)]] <- renderPlot({
+          create_modal_plots(input,
+                             dat,
+                             file = data_id,
+                             index = ii,
+                             threshold)
         })
-
-        # modals ####
-        output[["modals"]] <- renderUI({
-          create_modals(dat, file = data_id, threshold)
-        })
-
-
-        # plots in modals ####
-        for(i in 1:ncol(dat)){
-          local({
-            ii <- i
-            output[[paste0("plot",ii)]] <- renderPlot({
-              create_modal_plots(input, dat, file=data_id, index=ii, threshold)
-            })
-          })
-        }
       })
+    }
+  })
 }
 
 shinyApp(ui, server)
